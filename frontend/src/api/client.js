2305a6ebb5263cd5c7ae354d47403b6037e14dba
RONY MIGUEL SIERRA ROZO <46876038+ronysierra@users.noreply.github.com>
2025-12-01 02:54:15 +0000

    Configurando variable de entorno VITE_API_URL

--- frontend/src/api/client.js
+++ frontend/src/api/client.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const BASE_URL = 'https://literate-fortnight-9j5w9xr55q53wvp-8000.app.github.dev'; 
+const BASE_URL = import.meta.env.VITE_API_URL || 'https://literate-fortnight-9j5w9xr55q53wvp-8000.app.github.dev';
 
 const client = axios.create({
     baseURL: BASE_URL,
